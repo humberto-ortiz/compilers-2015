3b7e8018150669b10e6052d23091fdbf552c2a43
Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
2015-03-03 18:33:39 -0400

    Trying to understand first sets.

---
+++ first.py
@@ -0,0 +1,81 @@
+# Try to grok first
+# Copyright 2015 - Humberto Ortiz-Zuazaga <humberto.ortiz@upr.edu>
+
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from collections import defaultdict
+
+# Encode the grammar as a series of lines X->ABc
+# Capital letters are non-terminals
+# lowercase are terminals
+# no spaces
+# ~ is epsilon
+
+productions = """S->Bc
+S->DB
+B->ab
+B->cS
+D->d
+D->~"""
+
+# Look over the productions, construct sets of terminals and non-terminals
+terminals = set()
+nonterminals = set()
+
+for symbol in productions:
+    # lowercase letters are terminal symbols
+    if symbol.isupper():
+        nonterminals.update(symbol)
+    # uppercase letters are non-terminal symbols
+    if symbol.islower():
+        terminals.update(symbol)
+
+print nonterminals, terminals
+
+# start the algorithm
+
+# first is a dictionary of empty sets
+first = defaultdict(set)
+
+# add the terminals to their own first sets
+for symbol in terminals:
+    first[symbol].update(symbol)
+
+# epsilon is the tilde
+first["~"].update("~")
+
+# fixed point iteration
+for i in range(len(nonterminals)):
+    for symbol in nonterminals:
+        for line in productions.split():
+
+            X = line[0]
+            if X == symbol:
+                Y = line[3:] # skip "->"
+
+                if Y[0] == '~':
+                    # X derives epsilon
+                    first[X].update("~")
+
+                first[X].update(first[Y[0]] - set("~") )
+
+                # if prefix of Y is nullable
+                for j in range(len(Y) - 1):
+                    if "~" in first[Y[j]]:
+                        first[X].update(first[Y[j+1]] - set("~"))
+
+# We should be done now
+for symbol in sorted(nonterminals):
+    print symbol, first[symbol]
